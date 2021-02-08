007a61eb1b2a721b085b7c565ad7a8942da4d088
Ryan Kang <rkang2009@gmail.com>
2021-02-08 16:06:37 -0500

    new theme

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'https://kangxi11.github.io/CMPUT404-Lab5/'
 RELATIVE_URLS = False
 OUTPUT_PATH = "docs/"
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
