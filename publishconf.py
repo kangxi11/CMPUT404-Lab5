b7908aeebef1d32c475fcb22ae2960936a90bf23
Ryan Kang <rkang2009@gmail.com>
2021-02-08 15:59:08 -0500

    published

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,9 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://kangxi11.github.io/CMPUT404-Lab5/'
 RELATIVE_URLS = False
+OUTPUT_PATH = "docs/"
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
